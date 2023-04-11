a02ceee2155543443c0a3fd24272269a769c44ee
Robbie Gollan <robbiegollan@gmail.com>
2023-04-11 10:33:22 +1000

    Remove unnecessary component

--- src/components/Content.js
+++ src/components/Content.js
@@ -2,24 +2,30 @@ import { useState } from "react";
 
 import Card from "./UI/Card";
 import Tabs from "./Tabs";
-import Selections from "./Selections";
+import PotionEffects from "./Effects/PotionEffects";
+import Ingredients from "./Ingredients/Ingredients";
 
 export default function Content() {
-   const [viewingByPotions, setViewingByPotions] = useState(false);
+	const [viewingByPotions, setViewingByPotions] = useState(false);
 
-   const ingredientsClickHandler = () => {
-      setViewingByPotions(false);
-   }
+	const ingredientsClickHandler = () => {
+		setViewingByPotions(false);
+	};
 
-   const potionsClickHandler = () => {
-      setViewingByPotions(true);
-   }
+	const potionsClickHandler = () => {
+		setViewingByPotions(true);
+	};
 
 	return (
-		<div className="mx-auto grow">
+		<div className='mx-auto grow'>
 			<Card>
-            <Tabs viewingByPotions={viewingByPotions} ingredientsClickHandler={ingredientsClickHandler} potionsClickHandler={potionsClickHandler} />
-            <Selections viewingByPotions={viewingByPotions} />
+				<Tabs
+					viewingByPotions={viewingByPotions}
+					ingredientsClickHandler={ingredientsClickHandler}
+					potionsClickHandler={potionsClickHandler}
+				/>
+				{viewingByPotions && <PotionEffects />}
+				{!viewingByPotions && <Ingredients />}
 			</Card>
 		</div>
 	);
