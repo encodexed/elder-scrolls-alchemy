8995ee55d12a96ce0cc4009171875221cecae74a
Robbie Gollan <robbiegollan@gmail.com>
2023-04-14 16:52:52 +1000

    Change style, spacing

--- src/components/UI/SectionHeader.js
+++ src/components/UI/SectionHeader.js
@@ -1,6 +1,6 @@
 export default function SectionHeader(props) {
 	return (
-		<div className='flex px-2 mt-2 text-white bg-blue-500'>
+		<div className='flex px-2 mt-2 text-white bg-indigo-800'>
 			<div className='flex-grow my-auto text-left'>
             <h2 className='text-lg text-semibold'>{props.title}</h2>
             
